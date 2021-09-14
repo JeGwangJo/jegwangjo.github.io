ef42a69b4d7730f346bb52824c9a80a9a1c55d51
JeGwangJo <90168441+JeGwangJo@users.noreply.github.com>
2021-09-14 17:19:34 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,25 @@
 $(function(){
 /////////////////////start/////////////////////
 $("#myWeb").fullpage({
-    anchors:['m01', 'm02', 'm03', 'm04'],
-    //navigation: true,
+    anchors:['m00', 'm01', 'm02', 'm03', 'm04', 'm05'],
     afterLoad: function(origin, destination, direction){
-        //console.log(destination.index)
         var idx=destination.index;
         $(".section").eq(idx).addClass("on").siblings().removeClass("on")
         $("nav li").eq(idx).addClass("on").siblings().removeClass("on")
     },
-
-
 });
-
 
 $("#intro").YTPlayer({
     videoURL:'EZ7la-hMNuk',
-    containment:'.m01',
+    containment:'.m00',
     autoPlay:true,
     mute:true,
     showControls:false,
     playOnlyIfVisible:true,
     useOnMobile:true,
-    // realfullscreen:true,
-    // anchor:'center,center',
-    // ratio:'auto',
 });
-//pofol video control
+
+//pofol video control 01
 $("#portfolio_01").YTPlayer({
     containment:'.portfolio_01',
     autoPlay:true,
@@ -35,122 +28,55 @@ $("#portfolio_01").YTPlayer({
     optimizeDisplay:false,
     realfullscreen:true,
 });
-$(".m02 .xi-pause").on("click",function(){
+$(".m01 .xi-pause").on("click",function(){
     $('#portfolio_01').YTPPause();
 });
-$(".m02 .xi-play").on("click",function(){
+$(".m01 .xi-play").on("click",function(){
     $('#portfolio_01').YTPPlay();
 });
 
-/*slider_btn1*/
-$(".m03 .slide_icon1 .xi-pause").on("click",function(){
-    $('#video_01').YTPPause();
+//pofol video control 02
+$("#portfolio_02").YTPlayer({
+    containment:'.portfolio_02',
+    autoPlay:true,
+    mute:true,
+    showControls:false,
+    optimizeDisplay:false,
+    realfullscreen:true,
 });
-$(".m03 .slide_icon1 .xi-play").on("click",function(){
-    $('#video_01').YTPPlay();
+$(".m02 .xi-pause").on("click",function(){
+    $('#portfolio_02').YTPPause();
 });
-/*slider_bt2n*/
-$(".m03 .slide_icon2 .xi-pause").on("click",function(){
-    $('#video_02').YTPPause();
+$(".m02 .xi-play").on("click",function(){
+    $('#portfolio_02').YTPPlay();
 });
-$(".m03 .slide_icon1 .xi-play").on("click",function(){
-    $('#video_02').YTPPlay();
+
+//pofol video control 03
+$("#portfolio_03").YTPlayer({
+    containment:'.portfolio_03',
+    autoPlay:true,
+    mute:true,
+    showControls:false,
+    optimizeDisplay:false,
+    realfullscreen:true,
 });
-/*slider_btn3*/
-$(".m03 .slide_icon3 .xi-pause").on("click",function(){
-    $('#video_03').YTPPause();
+$(".m03 .xi-pause").on("click",function(){
+    $('#portfolio_03').YTPPause();
 });
-$(".m03 .slide_icon3 .xi-play").on("click",function(){
-    $('#video_03').YTPPlay();
-});
-/*slider_btn4*/
-$(".m03 .slide_icon4 .xi-pause").on("click",function(){
-    $('#video_04').YTPPause();
-});
-$(".m03 .slide_icon4 .xi-play").on("click",function(){
-    $('#video_04').YTPPlay();
-});
-/*slider_btn5*/
-$(".m03 .slide_icon5 .xi-pause").on("click",function(){
-    $('#video_05').YTPPause();
-});
-$(".m03 .slide_icon5 .xi-play").on("click",function(){
-    $('#video_05').YTPPlay();
-});
-/*slider_btn6*/
-$(".m03 .slide_icon6 .xi-pause").on("click",function(){
-    $('#video_06').YTPPause();
-});
-$(".m03 .slide_icon6 .xi-play").on("click",function(){
-    $('#video_06').YTPPlay();
-});
-/*slider_btn7*/
-$(".m03 .slide_icon7 .xi-pause").on("click",function(){
-    $('#video_07').YTPPause();
-});
-$(".m03 .slide_icon7 .xi-play").on("click",function(){
-    $('#video_07').YTPPlay();
+$(".m03 .xi-play").on("click",function(){
+    $('#portfolio_03').YTPPlay();
 });
 
 
-$("#video_01").YTPlayer({
-    containment:'#pf_video_01',
-    mute:true,
-    showControls:false,
-    playOnlyIfVisible:true,
-    optimizeDisplay:false,
-    
-});
-$("#video_02").YTPlayer({
-    containment:'#pf_video_02',
-    mute:true,
-    showControls:false,
-    playOnlyIfVisible:true,
-    optimizeDisplay:false,
-});
-$("#video_03").YTPlayer({
-    containment:'#pf_video_03',
-    mute:true,
-    showControls:false,
-    playOnlyIfVisible:true,
-    optimizeDisplay:false,
-});
-$("#video_04").YTPlayer({
-    containment:'#pf_video_04',
-    mute:true,
-    showControls:false,
-    playOnlyIfVisible:true,
-    optimizeDisplay:false,
-});
-$("#video_05").YTPlayer({
-    containment:'#pf_video_05',
-    mute:true,
-    showControls:false,
-    playOnlyIfVisible:true,
-    optimizeDisplay:false,
-});
-$("#video_06").YTPlayer({
-    containment:'#pf_video_06',
-    mute:true,
-    showControls:false,
-    playOnlyIfVisible:true,
-    optimizeDisplay:false,
-});
-$("#video_07").YTPlayer({
-    containment:'#pf_video_07',
-    mute:true,
-    showControls:false,
-    playOnlyIfVisible:true,
-    optimizeDisplay:false,
-});
-
-//photo_slider 슬라이드
+//video_slider 슬라이드
 $(".video_slider").slick({
     arrows:false,
     dots:true,
-    slidesToShow: 1,
-    autoplay: false,
-    //autoplaySpeed: 5000,
+    mute:true,
+    slidesToShow: 2,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    // centerMode:true,
     responsive: [
         {
           breakpoint: 768,
@@ -161,17 +87,25 @@ $(".video_slider").slick({
       ]
 });
 
+//video01
+$("#video01").YTPlayer({
+    containment:'self',
+    autoPlay:false,
+    mute:true,
+    showControls:true,
+    optimizeDisplay:false,
+});
 
-$(".m03 i.xi-angle-left-thin").on("click",function(){
+$(".m04 i.xi-angle-left-thin").on("click",function(){
     $(".video_slider").slick("slickPrev")
 })
-$(".m03 i.xi-angle-right-thin").on("click",function(){
+$(".m04 i.xi-angle-right-thin").on("click",function(){
     $(".video_slider").slick("slickNext")
 })
 
 
 //full windows open
-$("#header i, .m04 img").on("click", function(){
+$("#header i, .m05 img").on("click", function(){
     $("#full").addClass("on")
 })
 $("#full i , #full a").on("click", function(){
